b52c6675c191da002b4edd75385232c330644ee4
DavsonMelo <davsonmelo@protonmail.com>
2025-09-27 19:00:14 -0300

    Criação do board da home e implementação de algumas imagens

---
+++ src/components/board_assets/BoardAssets.tsx
@@ -0,0 +1,19 @@
+// Define imagens + posições relativas (%)
+export const boardAssets = {
+  park: {
+    src: '/board_home_icons/park.jpg',
+    top: '4.6%', // 10% da altura do tabuleiro
+    left: '0.7%', // 70% da largura do tabuleiro
+    size: '14.165%',
+    description: 'Chegou o fim de semana. Descanse por duas rodadas',
+  },
+  jail: {
+    src: '/board_home_icons/jail.jpg',
+    top: '4.6%',
+    left: '85.1%',
+    size: '14.2%',
+    description: 'Permaneça na cadeia até ... Regras',
+  },
+} as const;
+
+
